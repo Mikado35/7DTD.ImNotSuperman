7cce32bc73ab6fc87fb3ddde2a2db2895e1a3ea0
Mikado35 <36087166+Mikado35@users.noreply.github.com>
2021-09-12 10:06:33 +0200

    Version 1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# 7DTD.ModsA19
+Mods 7 Days to Die version A19
+
+zzzzzzzzzz 0-DMT Mikado I m not Superman
+No more pickup vehicles except bicycle.
---
+++ zzzzzzzzzz 0-DMT Mikado I m not Superman/Harmony/ImNotSuperman.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using DMT;
+using UnityEngine;
+
+
+namespace Harmony_Mikado_Core
+{
+    public class ImNotSupermanDMT
+    {
+        public class Init : IHarmony
+        {
+            public void Start()
+            {
+                Debug.Log((object)("Mikado Loading Patch : " + this.GetType().ToString()));
+                new Harmony(this.GetType().ToString()).PatchAll(Assembly.GetExecutingAssembly());
+            }
+        }
+
+        [HarmonyPatch(typeof(EntityVehicle))]
+        [HarmonyPatch("GetActivationCommands")]
+        private class PatchEntityVehicleGetActivationCommands
+        {
+            private static void Postfix(EntityVehicle __instance, EntityActivationCommand[] __result)
+            {
+                if (__instance.EntityClass.entityClassName.ToLower().Contains("bicycle") )
+                    return;
+                for (int index = 0; index < __result.Length; ++index)
+                {
+                    if (__result[index].text == "take")
+                        __result[index].enabled = false;
+                }
+            }
+        }
+    }
+
+}
